a6f1870a2aef177c4aba3c52417815db8473c320
github@mariaweb.fr <github@mariaweb.fr>
2018-12-08 16:03:41 -0500

    add releases

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 make TARGET="OMNIBUSF4PRO"
 make TARGET="MATEKF405OSD"
 make TARGET="MATEKF405SE"
+make TARGET="MATEKF411"
 make TARGET="MATEKF405"
 cp ./obj/*.hex ./releases/
+git add .
+git commit -m "add releases"
+git push origin master
