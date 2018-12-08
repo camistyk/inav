18edddddf0184d9519afd0da3357e7c32a7f47a7
github@mariaweb.fr <github@mariaweb.fr>
2018-12-08 16:31:28 -0500

    add releases

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ make TARGET="MATEKF405OSD"
 make TARGET="MATEKF405SE"
 make TARGET="MATEKF411"
 make TARGET="MATEKF405"
+make TARGET="CLRACINGF4AIR"
+make TRAGET="CLRACINGF4AIRV2"
 cp ./obj/*.hex ./releases/
 git add .
 git commit -m "add releases"
